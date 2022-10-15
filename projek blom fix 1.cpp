88ef3b2d59a3c1a03c0d2fc651dc3ee8104ea533
DiniSoraya <112565294+DiniSoraya@users.noreply.github.com>
2022-10-16 01:59:49 +0700

    Add files via upload

---
+++ projek blom fix 1.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+float hitungBMI (float tinggi, float berat)
+{
+	float bmi = berat/(tinggi*tinggi);
+	return bmi;
+}
+void display (float bmi)
+{
+	if (bmi<18.5) 
+	{
+		cout<<"Status nutrisi : Berat badan kurang "<<endl;
+	}	
+	else if (bmi>=18.5 && bmi<=24.9)
+	{
+		cout<<"Status nutrisi : Berat badan normal "<<endl;
+	}
+	else if (bmi>= 25.0 && bmi<=29.9)
+	{
+		cout<<"Status nutrisi : Pra-obesitas "<<endl;
+	}
+	else
+		cout<<"Status nutrisi: Obesitas "<<endl;
+	}
+
+int main ()
+{
+	float tinggi, berat, bmi;
+	int orang;
+	string nama [10];
+	float tertinggi = 0, terendah =100, totalBMI = 0;
+	
+	cout<<"Masukkan Jumlah Orang : ";
+	cin>> orang;
+	cout<<"----------------------------------------------------"<<endl;	
+	for (int i=0; i<orang; i++)
+	{
+		cout<<"Nama : ";
+		cin.ignore();
+		getline (cin, nama[i]);
+		cout<<"Tinggi (m) : ";
+		cin >> tinggi;
+		
+		cout << "Berat (Kg) : ";
+		cin >> berat;
+		
+		bmi = hitungBMI (tinggi, berat);
+		display (bmi);
+		
+		if (bmi>tertinggi)
+			tertinggi = bmi;
+		if (bmi<terendah)
+			terendah =bmi;
+		totalBMI = totalBMI + bmi;
+	cout<<"----------------------------------------------------"<<endl;
+	}
+	float rata = totalBMI/orang;
+	cout <<endl;
+	cout <<"Rata-rata nilai BMI dari "; for (int i=0; i<10; i++)  {
+	    cout<<nama [i]<<" , ";
+	}
+	cout<<" adalah " <<rata<< ", nilai BMI tertinggi dari meraka adalah " <<tertinggi<<", dan nilai BMI terendah dari mereka adalah " <<terendah<< endl;
+	return 0;
+}
